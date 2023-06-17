e61db4fd152c61fc35b8c5fe512be93927447bd6
Manuel Corporan <110692284+MCdev92@users.noreply.github.com>
2023-06-17 10:16:48 -0700

    called action

--- api/validator.go
+++ api/validator.go
@@ -1,10 +1,13 @@
 package api
 
-import "github.com/go-playground/validator/v10"
+import (
+	"github.com/go-playground/validator/v10"
+	"github.com/techschool/simplebank/util"
+)
 
 var validCurrency validator.Func = func(fieldLevel validator.FieldLevel) bool {
 	if currency, ok := fieldLevel.Field().Interface().(string); ok {
-		// check if currency is supported
+		return util.IsSupportedCurrency(currency)
 	}
 	return false
 }
